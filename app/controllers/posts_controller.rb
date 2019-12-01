10b23a13f344d785b854aa9a1f7f7e33895d0667
rhtaylor <randyisbusy@gmail.com>
2019-11-30 19:02:45 -0800

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,4 +12,18 @@ class PostsController < ApplicationController
   end
 
   # add create method here
+  
+    def create
+      @post = Post.new
+      @post.title = params[:title]
+      @post.description = params[:description]
+      @post.save 
+      redirect_to post_path(@post)
+    end
 end
+ 
+
+# _path just returns the path to the route
+# User avatar
+# Austin Melchior 4 MINUTES AGO
+# to the controller action
